0e9342862b08fcb16c0c955deb3f9bb785ae7546
aadyarajvanshi16 <aadya.rajvanshi16@gmail.com>
2026-08-09 20:27:33 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# ============================================
-# CAMERA CONFIGURATION
-# ============================================
-
-CAMERA_SOURCE = "webcam"
-
-RTSP_URL = ""
-
-RECONNECT_DELAY = 5
